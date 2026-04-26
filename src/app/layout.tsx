5b11e01e5271a4007883cc7b2fe09f146cb24129
Joshua Odelola <oladapoodelola@gmail.com>
2026-04-26 03:52:42 +0100

    feat: add site favicon, web manifest, and apple touch icon metadata to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Geist_Mono, Plus_Jakarta_Sans, Syne } from "next/font/google";
 import "./globals.css";
-import "./cabinet-grotesk.css";
 import { Footer } from "../components/footer";
 import { Navigation } from "../components/navigation";
 
@@ -33,6 +32,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <meta name="favicon" content="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+      </head>
       <body
         className={`${plusJakartaSans.variable} ${geistMono.variable} ${syne.variable} antialiased`}
       >
